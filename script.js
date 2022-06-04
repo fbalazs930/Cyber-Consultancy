8468c8a28245ed9a75659e5f0147313d19f4850b
fbalazs930 <fbalazs930@gmail.com>
2022-06-04 15:21:29 +0200

    fully responsive

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const menuIcon = document.querySelector('.menu-icon');
 let clicks = 0;
 navbarToggler.addEventListener('click', () => {
     if (clicks % 2 == 0) {
-        navbar.style.backgroundColor = '#49099390';
+        navbar.style.backgroundColor = '#490993cf';
         menuIcon.classList.add('fa-times');
         menuIcon.classList.remove('fa-bars');
     }
@@ -17,3 +17,20 @@ navbarToggler.addEventListener('click', () => {
     }
     clicks++;
 })
+
+window.addEventListener('wheel', checkScrollDirection);
+
+function checkScrollDirection(event) {
+  if (checkScrollDirectionIsUp(event)) {
+    navbar.style.opacity=1;
+  } else {
+    navbar.style.opacity=0;
+  }
+}
+
+function checkScrollDirectionIsUp(event) {
+  if (event.wheelDelta) {
+    return event.wheelDelta > 0;
+  }
+  return event.deltaY < 0;
+}
